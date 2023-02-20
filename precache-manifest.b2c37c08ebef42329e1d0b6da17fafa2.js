aa23a4d27870387a27c6c98c1b4b50584fa455ab
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2023-02-20 14:27:13 +0200

    Updates

---
+++ precache-manifest.b2c37c08ebef42329e1d0b6da17fafa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165020bcbca75c39e8b4ae85ea19e65a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "1c5f80b8e79e72e2d22a",
+    "url": "/todolist/static/css/main.4e2b4735.chunk.css"
+  },
+  {
+    "revision": "189b2bbcd94d71a18677",
+    "url": "/todolist/static/js/2.12a0c7d1.chunk.js"
+  },
+  {
+    "revision": "944a8acb954643b0ffbc3ccadcc433c1",
+    "url": "/todolist/static/js/2.12a0c7d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5f80b8e79e72e2d22a",
+    "url": "/todolist/static/js/main.8a01785f.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
